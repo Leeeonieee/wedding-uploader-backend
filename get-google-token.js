6f6b6503fa66868ac16087d0f5ab371850b7cd49
Leeeonieee <leonie.picker@hotmail.com>
2026-06-06 18:48:22 +1000

    Add files via upload

---
+++ get-google-token.js
@@ -0,0 +1,33 @@
+import readline from "readline";
+import { google } from "googleapis";
+
+const CLIENT_ID = "438035383089-tlb7jv2flpk6sb9i39vbd1mebj9or5ei.apps.googleusercontent.com";
+const CLIENT_SECRET = "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";
+const REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+
+const oauth2Client = new google.auth.OAuth2(
+  CLIENT_ID,
+  CLIENT_SECRET,
+  REDIRECT_URI
+);
+
+const SCOPES = ["https://www.googleapis.com/auth/drive.file"];
+
+const authUrl = oauth2Client.generateAuthUrl({
+  access_type: "offline",
+  scope: SCOPES,
+});
+
+console.log("Open this URL in your browser:");
+console.log(authUrl);
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("\nEnter the code from Google: ", async (code) => {
+  const { tokens } = await oauth2Client.getToken(code);
+  console.log("\nREFRESH TOKEN:\n", tokens.refresh_token);
+  rl.close();
+});
