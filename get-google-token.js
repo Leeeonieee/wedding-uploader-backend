f4d2c5210e2513ab8083d9e4d4ba3a46e8c91c25
Leeeonieee <leonie.picker@hotmail.com>
2026-06-28 17:41:30 +1000

    Update get-google-token.js

--- get-google-token.js
+++ get-google-token.js
@@ -3,7 +3,8 @@ import { google } from "googleapis";
 
 const CLIENT_ID = "438035383089-tlb7jv2flpk6sb9i39vbd1mebj9or5ei.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
-const REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+const REDIRECT_URI = "http://localhost";
+
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
